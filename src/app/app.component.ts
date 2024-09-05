dc42b85d5ae0d1eb0c86ed1301bf490761810bbd
watchlar2000 <sergii.koch@gmail.com>
2024-09-05 15:32:24 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'usense-test-assignment';
+  title = 'Usense test assignment';
 }
